d82e51027bc5876985866950cd362c33ac91304d
Umit Turk <uyt.turk@gmail.com>
2021-06-12 13:51:47 +0300

    login screen

--- firebase.js
+++ firebase.js
@@ -13,8 +13,8 @@ const app = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
   : firebase.app();
 
-  const db = app().firestore();
-  const auth = app().auth();
+  const db = app.firestore();
+  const auth = app.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
 
   export { db, auth, provider};
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import 'styled-components';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {auth, db} from '../firebase';
+import Login from './login';
 
 function MyApp({ Component, pageProps }) {
   const [user] = useAuthState(auth);
