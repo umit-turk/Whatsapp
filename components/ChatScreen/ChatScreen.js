880468515edf712bd4c2dbb5d1cffc08a0307b32
Umit Turk <uyt.turk@gmail.com>
2021-06-13 17:31:28 +0300

    chat screen component(1/3)

--- components/ChatScreen/ChatScreen.js
+++ components/ChatScreen/ChatScreen.js
@@ -1,15 +1,93 @@
+import { Avatar } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { auth, db } from "../../firebase";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import { IconButton } from "@material-ui/core";
+import { useCollection } from "react-firebase-hooks/firestore";
 
-function ChatScreen() {
-    return (
-        <Container>
-            <h1>This is a chat</h1>
-        </Container>
-    )
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("chats")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+
+  const showMessages = () => {
+      if(messagesSnapshot){
+          return messagesSnapshot.docs.map(message => (
+              <Message
+                key={message.id}
+                user={message.data().user}
+                message={{
+                    ...message.data(),
+                    timestamp: message.data().timestamp?.toDate().getTime(),
+                }}
+              />
+          ))
+      }
+  };
+  return (
+    <Container>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>Rec E mail</h3>
+          <p>Last seen...</p>
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+
+      <MessageContainer>
+        {/* show messages */}
+        <EndOfMessage />
+      </MessageContainer>
+    </Container>
+  );
 }
 
-export default ChatScreen
+export default ChatScreen;
 
-const Container = styled.div`
+const Container = styled.div``;
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
 
-`
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const EndOfMessage = styled.div``;
+const MessageContainer = styled.div``;
+
+const HeaderIcons = styled.div``;
